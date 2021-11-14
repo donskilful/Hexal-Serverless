6d34c2a4a01dbd39ee64525545065b72ed97ca2d
donskilful <donskilful@gmail.com>
2021-11-14 01:03:42 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          This Ui source code was cloned from a GitHub repo but everything about the AWS Serverless backend was done by me @ Donskilful
+        </p>
+      </div>
+    </footer>
+  )
+}
